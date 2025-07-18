d867f7819327b59920aaecfcef13f644630854e4
asaphho <asaphho@yahoo.com.sg>
2025-07-18 11:31:44 +0800

    Incorporated ChoiceHandler in GameContext

--- engine/contexts/game_context.py
+++ engine/contexts/game_context.py
@@ -1,16 +1,8 @@
 from engine.contexts.context import Context
+from engine.utils.choice_handler import ChoiceHandler
 from typing import Callable, TypeAlias
 
-ChoiceHandlerD: TypeAlias = dict[str, tuple[str, Callable[[Context, Context], bool]]]
-ChoiceHandlerDGenerator: TypeAlias = Callable[[dict], ChoiceHandlerD]
-ChoiceHandler: TypeAlias = dict[str, Callable[[Context, Context], bool]]
-
-
-def get_choice_handler_wo_display(choice_handler_w_display: ChoiceHandlerD) -> ChoiceHandler:
-    handler = {}
-    for choice in choice_handler_w_display:
-        handler[choice] = choice_handler_w_display[choice][1]
-    return handler
+ChoiceHandlerGenerator: TypeAlias = Callable[[dict], ChoiceHandler]
 
 
 class GameContext(Context):
@@ -20,8 +12,8 @@ class GameContext(Context):
     def __init__(self, game_state: dict,
                  entry_text_directory: dict[str, str],
                  entry_text_overrides: dict[str, str],
-                 default_choice_handler_generator: ChoiceHandlerDGenerator,
-                 choice_handler_overrides: dict[str, ChoiceHandlerDGenerator]):
+                 default_choice_handler_generator: ChoiceHandlerGenerator,
+                 choice_handler_overrides: dict[str, ChoiceHandlerGenerator]):
         super().__init__(parent_context=None, context_type='game', context_data=game_state)
         self.entry_text_directory = entry_text_directory
         self.entry_text_overrides = entry_text_overrides
@@ -41,15 +33,7 @@ class GameContext(Context):
         game_state = self.context_data
         encounter_key: str = game_state['encounter_key']
         if encounter_key in self.choice_handler_overrides:
-            choice_handler_w_display: ChoiceHandlerD = self.choice_handler_overrides[encounter_key](game_state)
+            return self.choice_handler_overrides[encounter_key](game_state)
         else:
-            choice_handler_w_display: ChoiceHandlerD = self.default_choice_handler_generator(game_state)
-        self.choices: dict[str, str] = {}
-        for choice in choice_handler_w_display:
-            self.choices[choice] = choice_handler_w_display[choice][0]
-        return get_choice_handler_wo_display(choice_handler_w_display)
+            return self.default_choice_handler_generator(game_state)
 
-    def print_choices(self) -> None:
-        # TODO: Add exit/save game choices
-        for choice in sorted((self.choices.keys())):
-            print(f'{choice}: {self.choices[choice]}')
