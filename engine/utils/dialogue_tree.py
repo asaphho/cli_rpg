54f90ca0880975dde80597ef8c2f5da51eb9235e
asaphho <asaphho@yahoo.com.sg>
2025-07-21 11:54:18 +0800

    WIP: DialogueTree.

--- engine/utils/dialogue_tree.py
+++ engine/utils/dialogue_tree.py
@@ -1,4 +1,18 @@
 class DialogueTree:
 
-    def __init__(self):
-        pass
+    def __init__(self, entry_text: str):
+        self.entry_text: str = entry_text
+        self.dialogue_paths: dict[str, tuple[str, str]] = {}
+
+    def add_node(self, node_name: str, player_line: str, response: str,
+                 from_node: str = None):
+        if '_' in node_name:
+            raise ValueError('Node names cannot have underscores.')
+        if node_name.strip() == '':
+            raise ValueError('Node name cannot be blank.')
+        full_node_name = node_name.strip() if from_node is None else f'{from_node}_{node_name.strip()}'
+        if full_node_name in self.dialogue_paths.keys():
+            raise ValueError(f'Node path {full_node_name} already taken.')
+        self.dialogue_paths[full_node_name] = (player_line, response)
+
+
