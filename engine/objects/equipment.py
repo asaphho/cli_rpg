e9525de7572e874a6667271d1328c6c5ba393c2b
asaphho <asaphho@yahoo.com.sg>
2025-07-22 09:55:50 +0800

    equipment and equipment loadout objects WIP

--- engine/objects/equipment.py
+++ engine/objects/equipment.py
@@ -2,10 +2,18 @@ from engine.objects.item import Item
 
 
 class Equipment(Item):
-	def __init__(self, item_id: str, display_name: str, slot_name: str, stackable: bool = False, weight: float = 0, quest_item: bool = False):
-		super().__init__(item_id=item_id, display_name=display_name, equippable=True, stackable=stackable, weight=weight, quest_item=quest_item)
+	def __init__(self, item_id: str, display_name: str, slot_name: str, stackable: bool = False, weight: float = 0,
+				 stack_size: int = 1, max_stack_size: int = 1, quest_item: bool = False, classification: str = 'equipment',
+				 equipment_classification: str = 'general_equipment'):
+		super().__init__(item_id=item_id, display_name=display_name, equippable=True, stackable=stackable, weight=weight,
+						 quest_item=quest_item, item_classification=classification, stack_size=stack_size,
+						 max_stack_size=max_stack_size)
 		self.slot = slot_name
+		self.equipment_classification = equipment_classification
 
 	def get_slot(self) -> str:
 		return self.slot
+
+	def get_equipment_classification(self) -> str:
+		return self.equipment_classification
 
--- engine/objects/equipment_loadout.py
+++ engine/objects/equipment_loadout.py
@@ -3,7 +3,7 @@ from engine.objects.equipment import Equipment
 
 
 class EquipmentLoadout:
-	def __init__(self, slot_names: list[str]=None):
+	def __init__(self, slot_names: list[str] = None):
 		cleaned_slot_names = [name.lower().strip() for name in slot_names] if slot_names is not None else []
 		if 'main_hand' in cleaned_slot_names or 'off_hand' in cleaned_slot_names:
 			raise ValueError('\'main_hand\' and \'off_hand\' are reserved names.')
@@ -12,6 +12,30 @@ class EquipmentLoadout:
 		self.currently_equipped: dict[str, Union[Equipment, None]] = {'main_hand': None, 'off_hand': None}
 		for slot in slot_names:
 			self.currently_equipped[slot] = None
+
+	def create_slot(self, slot_name: str) -> None:
+		if slot_name.lower().strip() in self.currently_equipped:
+			raise ValueError('Slot name already exists')
+		self.currently_equipped[slot_name.lower().strip()] = None
+
+	def equip(self, equipment: Equipment) -> None:
+		slot = equipment.get_slot()
+		if slot not in self.currently_equipped:
+			raise ValueError(f'Slot {slot} does not exist')
+		self.currently_equipped[slot] = equipment
+
+	def unequip(self, slot: str) -> None:
+		if slot.lower().strip() in self.currently_equipped:
+			self.currently_equipped[slot.lower().strip()] = None
+
+	def remove_slot(self, slot: str) -> None:
+		if slot.lower().strip() in ('main_hand', 'off_hand'):
+			raise ValueError('Slot cannot be removed')
+		if slot.lower().strip() in self.currently_equipped:
+			self.currently_equipped.pop(slot.lower().strip())
+		else:
+			raise ValueError(f'Slot \'{slot.lower().strip()}\' does not exist')
+
 
 
 		
