474d3ae36e61965c5e76591bf6ec84a8d3a1df24
asaphho <asaphho@yahoo.com.sg>
2025-07-23 10:17:28 +0800

    Added description argument

--- engine/objects/equipment.py
+++ engine/objects/equipment.py
@@ -4,10 +4,10 @@ from engine.objects.item import Item
 class Equipment(Item):
 	def __init__(self, item_id: str, display_name: str, slot_name: str, stackable: bool = False, weight: float = 0,
 				 stack_size: int = 1, max_stack_size: int = 1, quest_item: bool = False, classification: str = 'equipment',
-				 equipment_classification: str = 'general_equipment'):
+				 equipment_classification: str = 'general_equipment', description: str = ''):
 		super().__init__(item_id=item_id, display_name=display_name, equippable=True, stackable=stackable, weight=weight,
 						 quest_item=quest_item, item_classification=classification, stack_size=stack_size,
-						 max_stack_size=max_stack_size)
+						 max_stack_size=max_stack_size, description=description)
 		self.slot = slot_name.lower().strip()
 		self.equipment_classification = equipment_classification.lower().strip()
 
